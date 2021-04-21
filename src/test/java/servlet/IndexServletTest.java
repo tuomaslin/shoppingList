9c61ea45a44aa7d684d410a0ae1fc01942d35829
Teemu H <swd1tn002@users.noreply.github.com>
2021-04-21 11:42:28 +0300

    Palvelintestien uusi TestServer-luokka

--- src/test/java/servlet/IndexServletTest.java
+++ src/test/java/servlet/IndexServletTest.java
@@ -1,57 +1,46 @@
-package launch;
+package servlet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class StartServerTest {
+import testserver.TestServer;
 
-    private static final int TEST_PORT = 8888;
-    private static final String SERVER_ROOT = "http://localhost:" + TEST_PORT;
-
-    private static HttpClient client = HttpClient.newHttpClient();
-    private static Tomcat server;
+public class IndexServletTest {
 
     @BeforeAll
     public static void startServer() throws LifecycleException {
-        server = Main.createServer(TEST_PORT);
-        server.start();
+        TestServer.start();
     }
 
     @AfterAll
     public static void stopServer() throws LifecycleException {
-        server.stop();
+        TestServer.stop();
     }
 
     @Test
-    public void frontPageReturnsHttp200() throws IOException, InterruptedException {
-        HttpResponse<String> response = makeRequest("/");
+    public void frontPageReturnsHttp200() {
+        HttpResponse<String> response = TestServer.get("/");
 
         assertEquals(200, response.statusCode());
     }
 
     @Test
-    public void frontPageContainsCorrectText() throws IOException, InterruptedException {
-        HttpResponse<String> response = makeRequest("/");
+    public void frontPageContainsCorrectText() {
+        HttpResponse<String> response = TestServer.get("/");
 
         assertTrue(response.body().contains("Congratulations"));
     }
 
     @Test
-    public void tomcatReturnsPagesWithUtf8Encoding() {
-        HttpResponse<String> response = makeRequest("/");
+    public void pagesHaveUtf8Encoding() {
+        HttpResponse<String> response = TestServer.get("/");
 
         String contentType = response.headers().firstValue("Content-Type").get().toLowerCase();
 
@@ -60,21 +49,9 @@ public class StartServerTest {
     }
 
     @Test
-    public void nonexistingPathsReturnHttp404() throws IOException, InterruptedException {
-        HttpResponse<String> response = makeRequest("/this-is-not-found");
+    public void nonexistingPathsReturnHttp404() {
+        HttpResponse<String> response = TestServer.get("/this-is-not-found");
 
         assertEquals(404, response.statusCode());
-    }
-
-    private static HttpResponse<String> makeRequest(String url) {
-        URI uri = URI.create(SERVER_ROOT + url);
-
-        try {
-            HttpRequest request = HttpRequest.newBuilder(uri).build();
-            return client.send(request, BodyHandlers.ofString());
-
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
